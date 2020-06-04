123e2ac430d5dc9027f08c0dcc62417f14e476b0
vnzholud <vnzholud@mail.ru>
2020-06-04 18:00:44 +0600

    Программа работы клиент/сервер

---
+++ ClientServer.java
@@ -0,0 +1,66 @@
+package dz04062020;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientServer {
+
+    public static void main(String[] args) {
+        ServerSocket server = null;                //Ждущий сервер
+        Socket socket = null;              // Клиентский комп
+
+        try {
+            server = new ServerSocket(8189); // открываем свободный порт и обрабатываем исключения если что
+            System.out.println("сервер запущен");
+
+            socket = server.accept(); //передаем клиенту сокет для подключения
+            System.out.println("Клиент подключился");
+
+            Scanner inServer = new Scanner(socket.getInputStream());  // Сканером слушаем ввод от сервера
+
+            Scanner inClient = new Scanner(System.in);  // Сканером слушаем ввод от клиента
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true); // Более удобный метод для ввода
+
+
+
+            // Бесконечным циклом слушаем сервер и клиентский сокет
+
+            while (true){
+
+                String strServer = inServer.nextLine();
+
+                String strClient = inClient.nextLine();
+
+
+                if (strServer.equals("end")){
+                    System.out.println("Сервер отключился");
+                    break;
+
+                }
+
+                System.out.println("Сервер: "+ strServer);
+                out.println("Клиент: "+strClient);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   finally {   //Осуществляем закрытие сокета
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {  //Осуществляем закрытие сервера
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
